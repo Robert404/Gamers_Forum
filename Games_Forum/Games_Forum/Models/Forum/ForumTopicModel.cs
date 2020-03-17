55bdc9e63228eb7cc98d1fb58a2e3c9c21800a37
Robert <robners404@gmail.com>
2020-03-17 13:09:09 +0400

    Start adding new forums and posts

--- Games_Forum/Games_Forum/Models/Forum/ForumTopicModel.cs
+++ Games_Forum/Games_Forum/Models/Forum/ForumTopicModel.cs
@@ -10,6 +10,6 @@ namespace Games_Forum.Models.Forum
     {
         public ForumListingModel Forum { get; set; }
         public IEnumerable<PostListingModel> Posts { get; set; }
-
+        public string SearchQuery { get; set; }
     }
 }
