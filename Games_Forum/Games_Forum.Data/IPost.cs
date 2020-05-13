6c36118772f65a98f489128aaea63eff4cb24e15
Robert <robners404@gmail.com>
2020-05-13 14:31:01 +0400

    GetAllPosts, GetById Unit Test

--- Games_Forum/Games_Forum.Data/IPost.cs
+++ Games_Forum/Games_Forum.Data/IPost.cs
@@ -9,7 +9,7 @@ namespace Games_Forum.Data
     {
         Post GetById(int id);
         IEnumerable<Post> GetAll();
-        IEnumerable<Post> GetFilteredPosts(Forum forum ,string searchQuery);
+        IEnumerable<Post> GetFilteredPosts(Forum forum, string searchQuery);
         IEnumerable<Post> GetFilteredPosts(string searchQuery);
         IEnumerable<Post> GetPostsByForum(int id);
         IEnumerable<Post> GetLatestPosts(int n);
--- Games_Forum/Games_Forum.Tests/PostServiceTests.cs
+++ Games_Forum/Games_Forum.Tests/PostServiceTests.cs
@@ -2,6 +2,7 @@
 using Games_Forum.Data.Models;
 using Games_Forum.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using System;
 using System.Linq;
@@ -14,14 +15,14 @@ namespace Games_Forum.Tests
         [TestCase("coffee", 3)]
         [TestCase("posT", 1)]
         [TestCase("tEa", 1)]
-        [TestCase("Corona",0)]
-        public void Return_Filtered_Results_Similar_To_Query(string query, int expected) 
+        [TestCase("Corona", 0)]
+        public void Return_Filtered_Results_Similar_To_Query(string query, int expected)
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
 
             //Arrange
-            using (var ctx = new ApplicationDbContext(options)) 
+            using (var ctx = new ApplicationDbContext(options))
             {
                 ctx.Forums.Add(new Forum
                 {
@@ -54,7 +55,7 @@ namespace Games_Forum.Tests
 
                 ctx.SaveChanges();
 
-                
+
             }
 
             //Act
@@ -68,5 +69,85 @@ namespace Games_Forum.Tests
                 Assert.AreEqual(expected, postCount);
             }
         }
+
+
+        [Test]
+        public void Return_Post_Correctly_By_PostId()
+        {
+
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Get_Post_By_Id").Options;
+
+            using (var ctx = new ApplicationDbContext(options))
+            {
+                ctx.Posts.Add(new Post
+                {
+                    Id = 1986,
+                    Title = "Test Post",
+                });
+
+                ctx.Posts.Add(new Post
+                {
+                    Id = 1522,
+                    Title = "Another Test",
+                });
+                ctx.SaveChanges();
+            }
+
+
+            //Act
+            using (var ctx = new ApplicationDbContext(options))
+            {
+                var postService = new PostService(ctx);
+                var post = postService.GetById(1986);
+
+                //Assert
+                Assert.AreEqual(post.Title, "Test Post");
+            }
+
+        }
+
+        [Test]
+        public void Get_All_Posts_Equal_To_GetAll() 
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Get_Post_By_Id").Options;
+
+            using (var ctx = new ApplicationDbContext(options)) 
+            {
+                ctx.Add(new Post
+                {
+                    Id = 2551,
+                    Title = "first Title"
+                });
+
+                ctx.Add(new Post
+                {
+                    Id = 600,
+                    Title = "secont Title"
+                });
+
+                ctx.Add(new Post
+                {
+                    Id = 660,
+                    Title = "third Title"
+                });
+
+                ctx.SaveChanges();
+            }
+
+            //Act
+            using (var ctx = new ApplicationDbContext(options)) 
+            {
+                var postService = new PostService(ctx);
+                var posts = postService.GetAll();
+
+                //Arrange
+                Assert.AreEqual(posts.Count(), 3);
+            }
+        }
     }
 }
+
